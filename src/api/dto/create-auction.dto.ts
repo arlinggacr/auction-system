33862f572be780b490b532d220a834236ca3f43b
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-06-23 22:53:11 +0700

    add: adding create auction DTO

---
+++ src/api/dto/create-auction.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateAuctionDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @Min(0)
+  startPrice: number;
+
+  @IsDateString()
+  endTime: Date;
+}
