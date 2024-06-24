481f56838cfa222d74a4e152ec7d570822d71286
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-06-24 20:09:34 +0700

    change: update the id into string because implement the uuid

--- src/domain/repositories/auction.repositories.ts
+++ src/domain/repositories/auction.repositories.ts
@@ -2,6 +2,6 @@ import { Auction } from '../entities/auction.model';
 
 export interface AuctionRepository {
   findAll(): Promise<Auction[]>;
-  findById(id: number): Promise<Auction>;
+  findById(id: string): Promise<Auction>;
   save(auction: Auction): Promise<Auction>;
 }
