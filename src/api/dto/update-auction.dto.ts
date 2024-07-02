3326423f87089116d2bf913d1a2dc8420ba70623
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-07-02 12:39:45 +0700

    add: add dto for update

---
+++ src/api/dto/update-auction.dto.ts
@@ -0,0 +1,48 @@
+import { Optional } from '@nestjs/common';
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class UpdateAuctionDto {
+  @Optional()
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @Optional()
+  @IsString()
+  description: string;
+
+  @Optional()
+  @IsNumber()
+  @Min(0)
+  startPrice: number;
+
+  @Optional()
+  @IsNumber()
+  @Min(0)
+  currentPrice: number;
+
+  @Optional()
+  @IsDateString()
+  endTime: Date;
+
+  @Optional()
+  @IsNumber()
+  @Min(1)
+  buyNowPrice: number;
+
+  @Optional()
+  @IsNumber()
+  @Min(500)
+  jumpBid: number;
+
+  @Optional()
+  @IsBoolean()
+  isClosed: boolean;
+}
